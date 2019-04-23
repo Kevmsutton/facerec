c1d87dc1db5f583f2c811ed09f60703b2bdbfd38
Kevmsutton <kevmsutton@gmail.com>
2019-04-23 14:56:20 +0100

    added particles.js, and some extras

--- src/App.js
+++ src/App.js
@@ -3,14 +3,45 @@ import "./App.css";
 import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/Logo/Logo";
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
+import Rank from "./Components/Rank/Rank";
+import Particles from "react-particles-js";
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 200,
+      density: {
+        enable: true,
+        value_area: 1200
+      }
+    }
+  }
+};
 
 class App extends Component {
+  state = {
+    input: ""
+  };
+
+  onInputChange = event => {
+    console.log(event.target.value);
+  };
+
+  onSubmit = () => {
+    console.log("click");
+  };
+
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particleOptions} />
         <Navigation />
         <Logo />
-        <ImageLinkForm />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onSubmit={this.onSubmit}
+        />
         {/* <FaceRecognition /> */}
       </div>
     );
