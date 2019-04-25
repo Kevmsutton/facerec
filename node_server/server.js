57b5eda8ed854b9a9dbcc9d180173e69b9d10233
Kevmsutton <kevmsutton@gmail.com>
2019-04-25 17:57:43 +0100

    basic learn node server

--- node_server/server.js
+++ node_server/server.js
@@ -3,17 +3,91 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "john",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: "0",
+      joined: new Date()
+    },
+    {
+      id: "124",
+      name: "sally",
+      email: "sally@gmail.com",
+      password: "bans",
+      entries: "0",
+      joined: new Date()
+    }
+  ]
+};
+
 app.get("/", (req, res) => {
-  res.send("getting root");
+  res.send(database.users);
 });
 
-app.post("/profile", (req, res) => {
-  console.log(req.body);
-  res.send("success");
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
+  }
 });
 
-app.listen(3000);
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    entries: "0",
+    joined: new Date()
+  });
+  res.json(database.users[database.users.length - 1]);
+});
 
-// req.headers
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
+});
+
+app.put("/image/", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user.entries++);
+    }
+  });
+  if (!found) {
+    res.status(400).json("user not found");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("running on port 3000");
+});
+
+// / --> res = this is working
+// /signin --> POST = success/fail
+// /register --> POST = user object
+// /profile/:userId --> GET = user
+// /image --> PUT = user count++
